e65a1530b99541a2f9999ad520330b2b3c45be0c
Coshido <coshido@gmail.com>
2023-01-08 18:12:45 +0100

    add debugging elements

--- javascript.js
+++ javascript.js
@@ -1,31 +1,31 @@
 const Player = () => {
-  const playerClick = () => {
-    console.log("player click");
+  const playerClick = (num) => {
+    let asd = console.log("player click", Gameboard.player, num);
   };
   return { playerClick };
 };
 
 const Gameboard = (() => {
-  const gameboard = [" ", " ", "", "", "", "", "", "", ""];
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
   const displayDiv = document.querySelector(".gameboard");
   const player = Player();
   const render = () => {
     displayDiv.innerHTML = "";
-    console.log("asd", gameboard);
     for (let i = 0; i < gameboard.length; i++) {
       let div = document.createElement("div");
       div.classList.add("square");
+      div.setAttribute("number", i);
       div.innerHTML = gameboard[i];
-      div.addEventListener("click", player.playerClick);
+      div.addEventListener("click", player.playerClick.bind(this, i));
       displayDiv.appendChild(div);
     }
   };
-  return { gameboard, render };
+  return { gameboard, render, player };
 })();
 
 const GameController = (() => {
-  //const gameboard = Gameboard.gameboard;
-  //const player1 = Player();
+  const player1 = Player();
+  const player2 = Player();
   Gameboard.render();
-  return {};
+  return { player1, player2 };
 })();
