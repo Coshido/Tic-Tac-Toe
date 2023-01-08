33e2a9eaba5c211de13d59a4530585a309883790
Coshido <coshido@gmail.com>
2023-01-08 13:48:16 +0100

    add player click logic

--- javascript.js
+++ javascript.js
@@ -1,15 +1,33 @@
+const Player = () => {
+  const playerClick = () => {
+    console.log("player click");
+  };
+  return { playerClick };
+};
+
 const Gameboard = (() => {
-  const gameboard = [];
+  const gameboard = [" ", " ", "", "", "", "", "", "", ""];
 
   return { gameboard };
 })();
 
-const DisplayController = (() => {
-    const displayDiv = document.querySelector('.gameboard');
-    const render = 
+const GameController = (() => {
+  const gameboard = Gameboard.gameboard;
+  const displayDiv = document.querySelector(".gameboard");
+  const player1 = Player();
+  const render = () => {
+    displayDiv.innerHTML = "";
+    console.log("asd", gameboard);
+    for (let i = 0; i < gameboard.length; i++) {
+      let div = document.createElement("div");
+      div.classList.add("square");
+      div.innerHTML = gameboard[i];
+      div.addEventListener("click", player1.playerClick);
+      displayDiv.appendChild(div);
+    }
+  };
+
+  return { render };
 })();
 
-const Player = () => {
-  return {};
-};
-
+GameController.render();
